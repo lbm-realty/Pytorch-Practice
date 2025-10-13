2cc2ece20c22ff9460a6de4c3f3ae0a446acc57e
lbm-realty <labeebmuntasir@gmail.com>
2025-10-13 15:55:01 -0500

    Design made more Object Oriented

--- level7.py
+++ level7.py
@@ -47,19 +47,52 @@ test_dataset = datasets.MNIST(
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-model = SimpleNN()
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+class Trainer:
+    def __init__(self, model, train_loader, test_loader, criterion, optimizer):
+        self.model = model
+        self.train_loader = train_loader
+        self.test_loader = test_loader
+        self.criterion = criterion
+        self.optimizer = optimizer
 
-epochs = 5
+    def train(self):
+        self.model.train()
+        curr_loss = 0
+        for images, labels in self.train_loader:
+            outputs = self.model(images)
+            loss = self.criterion(outputs, labels)
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+            curr_loss += loss.item()
+        return curr_loss / len(self.train_loader)
+                
+    def test(self):
+        self.model.eval()
+        correct, total = 0, 0
+        pred = torch.tensor([])
+        label = []
+        with torch.no_grad():
+            for images, labels in self.test_loader:
+                outputs = self.model(images)
+                unknown, predicted = torch.max(outputs, 1)
+                pred = unknown
+                total += labels.size(0)
+                label = labels
+                print(labels.shape, predicted.shape)
+                correct += (predicted == labels).sum().item()
+        print(pred)
+        return correct / total
 
-for epoch in range(epochs):
-    for images, labels in train_loader:
-        outputs = model(images)
-        loss = criterion(outputs, labels)
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+if __name__ == "__main__":
+    model = SimpleNN()
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    trainer = Trainer(model, train_loader, test_loader, criterion, optimizer)
+    epochs = 1
 
-    print(f"Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}")
+    for epoch in range(epochs):
+        train_loss = trainer.train()
+        test_accuracy = trainer.test()
+        print(f"Epoch [{epoch+1}/{epochs}], Loss: {train_loss:.4f}, Test: {test_accuracy:.4f}")
